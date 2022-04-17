0cdadb4d249d2f8c4fd6dcb775113d82dd3d5c45
RAMMOHAN KAMMA <62167758+RAMMOHANKAMMA@users.noreply.github.com>
2022-04-17 13:18:59 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Program {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int a,b,c=0;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("The sum is " + c);
+	}
+}
+
